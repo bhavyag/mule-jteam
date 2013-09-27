b4ff5b5999366ca15201082eb7124de8b7e899f8
cheapeone@gmail.com <cheapeone@gmail.com@d9f27457-34bd-f182-1350-a7f1b5f6f8a4>
2013-09-27 04:51:31 +0000

    first try with my method and readme.p2.txt, not sure if the commit will work, this is a test

--- src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -63,8 +63,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("902812043");
 		  //Person2 replace P2 with your name
 		  //and ggg000x with your gt num
-		  Person2 p2 = new Person2("P2");
-		  toPrint += p2.toString("ggg002x");
+		  Person2 p2 = new Person2("Christopher");
+		  toPrint += p2.toString("902916424");
 		  //Person3 replace P3 with your name
 		  //and ggg000x with your gt num
 		  Person3 p3 = new Person3("P3");
